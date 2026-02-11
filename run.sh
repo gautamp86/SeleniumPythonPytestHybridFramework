da44b3a0fa8279b895058b6ff6a3289dfbe6cbd4
gautamp86 <gautamp86@gmail.com>
2026-02-11 11:59:00 +0530

    corrected run.sh file

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ cd /Users/snehalpophali/Desktop/pythonautomation/SeleniumHybridFramework
 source /Users/snehalpophali/Desktop/pythonautomation/SeleniumHybridFramework/venv/bin/activate
 
 # Run only sanity tests
+source venv/bin/activate
 python3 -m pytest -v -s -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 
 # Run sanity OR regression tests
