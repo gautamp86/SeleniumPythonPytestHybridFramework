e464a79df4eebd9ef3fdb512ca2ddb35d8fa957e
gautamp86 <gautamp86@gmail.com>
2026-02-11 12:02:01 +0530

    corrected run.sh file....

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /Users/snehalpophali/Desktop/pythonautomation/SeleniumHybridFramework
-
-# Activate virtual environment using absolute path
-source /Users/snehalpophali/Desktop/pythonautomation/SeleniumHybridFramework/venv/bin/activate
-
 # Run only sanity tests
 source venv/bin/activate
 python3 -m pytest -v -s -m "sanity" --html=Reports/report.html testCases/ --browser chrome
